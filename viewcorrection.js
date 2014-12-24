7bc254c400254ddbc21362da02c58268bab0abb6
sravan546 <sravan_546@yahoo.com>
2014-12-23 23:55:41 -0700

    test cases

--- viewcorrection.js
+++ viewcorrection.js
@@ -1,5 +1,21 @@
 var treemodel = require("./treeModel.js");
 
+//test cases
+//6 posts
+//more than 6 posts
+//all max cases
+//all min cases
+//all mid size cases
+//mid and 2max
+//mid and min's
+//3 max cases
+//4 max cases
+//5 max cases
+//6 max cases
+//3,4,5,6 near max case
+
+//boundry conditions
+
 module.exports.ViewCorrecter= function(){
 
 	this.correctViews = function(treemodel){
